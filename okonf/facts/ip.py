804c8bbe7b3214729d566e23ff6d623617a6d6be
Hugo Herter <git@hugoherter.com>
2017-08-29 12:05:49 +0200

    Remove old deprecated code

--- okonf/facts/ip.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-async def interface_ipv4(host, name):
-    status = await host.run("ip address show {}".format(name))
-    ip = re.findall('inet ([\d\.]+)', status)[0]
-    return ip
--- okonf/facts/lxd.py
+++
@@ -1,5 +0,0 @@
-from pylxd import Client
-
-
-async def container_is_present(lxdhost, name):
-    return lxdhost.containers.exists(name)
