36e88c1a77976af86f9fc8d77987a0b78f3e8964
Hugo Herter <git@hugoherter.com>
2017-08-29 12:05:03 +0200

    Add git module

---
+++ okonf/modules/git.py
@@ -0,0 +1,29 @@
+import logging
+
+from okonf.modules.abstract import Module
+from okonf.modules.files import DirectoryPresent
+
+
+class GitClone(Module):
+
+    def __init__(self, repository: str, directory: str, branch: str='master'):
+        self.repository = repository
+        self.directory = directory
+        self.branch = branch
+
+    async def get_branch(self, host):
+        if not await DirectoryPresent(self.directory).check(host):
+            logging.debug("Git directory absent: {}".format(self.directory))
+            return False
+        command = "git -C {} rev-parse --abbrev-ref HEAD".format(self.directory)
+        branch_name = await host.run(command)
+        return branch_name.strip()
+
+    async def check(self, host):
+        branch = await self.get_branch(host)
+        return branch == self.branch
+
+    async def apply(self, host):
+        await host.run("git clone {} {}".format(self.repository, self.directory))
+        return True
+
