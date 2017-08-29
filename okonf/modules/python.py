f89e654e2a3c6dfb034df815f2be3a5ec8f417b9
Hugo Herter <git@hugoherter.com>
2017-08-29 23:42:26 +0200

    Add pip capability in 'python' module

--- okonf/modules/python.py
+++ okonf/modules/python.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 from okonf.modules.abstract import Module
 from okonf.modules.files import FilePresent
 
@@ -31,5 +33,53 @@ class Virtualenv(Module):
         command.append(self.path)
         command = ' '.join(command)
 
+        await host.run(command)
+        return True
+
+
+class PipInstalled(Module):
+
+    def __init__(self, packages: Iterable[str], virtualenv: str=None,
+                 latest: bool=False):
+        self.packages = packages
+        self.virtualenv = virtualenv
+        self.latest = latest
+
+    async def info(self, host):
+        if self.virtualenv:
+            command = "{}/bin/pip freeze".format(self.virtualenv)
+        else:
+            command = "pip freeze"
+        output = await host.run(command)
+        lines = (
+            line.split('==', 1)
+            for line in output.strip().split('\n')
+        )
+        return {
+            key.lower(): value
+            for key, value in lines
+        }
+
+    async def check(self, host):
+        installed = await self.info(host)
+        for pkg in self.packages:
+            if '==' in pkg:
+                # Version specified
+                pkg, version = pkg.split('==')
+                if version != installed.get(pkg.lower(), False):
+                    return False
+            else:
+                # No version specified
+                if pkg.lower() not in installed:
+                    return False
+        return True
+
+    async def apply(self, host):
+        if self.virtualenv:
+            pip = "{}/bin/pip".format(self.virtualenv)
+        else:
+            pip = 'pip'
+
+        command = "{} install {}".format(pip, ' '.join(self.packages))
         await host.run(command)
         return True
