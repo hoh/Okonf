d58b688460aa0f40bc7ce3502cbd8b520a94b1b8
Hugo Herter <git@hugoherter.com>
2017-09-08 12:34:30 +0200

    Add module Users with GroupMember

---
+++ okonf/modules/users.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from okonf.modules.abstract import Module
+
+
+class GroupMember(Module):
+    """Ensure that a user is member of a group"""
+
+    def __init__(self, username: str, group: str) -> None:
+        self.username = username
+        self.group = group
+
+    async def info(self, host):
+        command = "groups {}".format(self.username)
+        result = await host.run(command, check=False)
+
+        prefix = "{} : ".format(self.username)
+        assert result.startswith(prefix)
+        return result[len(prefix):].split()
+
+    async def check(self, host):
+        info = await self.info(host)
+        return self.group in info
+
+    async def apply(self, host):
+        await host.run("sudo adduser {} {}".format(self.username, self.group))
+        return True
