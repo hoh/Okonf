b6d35ea20ed22a2b6390031217fb887cc00088bc
Hugo Herter <git@hugoherter.com>
2017-09-08 12:33:59 +0200

    Fix GitClone when different branch was checked-out

--- okonf/modules/git.py
+++ okonf/modules/git.py
@@ -6,7 +6,7 @@ from okonf.modules.files import DirectoryPresent
 
 class GitClone(Module):
 
-    def __init__(self, repository: str, directory: str, branch: str='master'):
+    def __init__(self, repository: str, directory: str, branch: str=None):
         self.repository = repository
         self.directory = directory
         self.branch = branch
@@ -22,7 +22,10 @@ class GitClone(Module):
 
     async def check(self, host):
         branch = await self.get_branch(host)
-        return branch == self.branch
+        if branch and not self.branch:
+            return True
+        else:
+            return branch == self.branch
 
     async def apply(self, host):
         await host.run("git clone {} {}"
