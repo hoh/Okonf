8e3827996eadb18886ebc223167a36a8243714ac
Hugo Herter <git@hugoherter.com>
2017-10-28 14:48:51 +0200

    Add module LXD

---
+++ okonf/modules/lxd.py
@@ -0,0 +1,26 @@
+import json
+
+from okonf.modules.abstract import Module
+
+
+class Container(Module):
+
+    def __init__(self, name: str, image: str='images:debian/stretch') -> None:
+        self.name = name
+        self.image = image
+
+    async def info(self, host):
+        command = "lxc list --format=json"
+        result = json.loads(await host.run(command))
+        existing = [container for container in result
+                    if container['name'] == self.name]
+        return existing[0] if existing else None
+
+    async def check(self, host):
+        existing = await self.info(host)
+        return existing is not None
+
+    async def apply(self, host):
+        await host.run("lxc launch {} {}"
+                       .format(self.image, self.name))
+        return True
