d730e86e6b43be637e51a75f71d690065484984f
Hugo Herter <git@hugoherter.com>
2017-10-28 14:49:30 +0200

    Remove function run_debug from utils

--- okonf/utils.py
+++ okonf/utils.py
@@ -5,22 +5,10 @@ import asyncio
 import subprocess
 
 
-def run_debug(tasks):
-    loop = asyncio.get_event_loop()
-    loop.set_debug(True)
-    for task in tasks:
-        loop.run_until_complete(task)
-        print('*' * 20)
-    loop.close()
-
-
 def run(task, debug=False):
-    if debug or '--debug' in sys.argv:
-        return run_debug(task)
-    else:
-        loop = asyncio.get_event_loop()
-        result = loop.run_until_complete(task)
-        return result
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(task)
+    return result
 
 
 def get_local_file_hash(file_path):
