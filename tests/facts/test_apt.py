e1bec4c33d44156ce9e9cc38b8fc782460afccf7
Hugo Herter <git@hugoherter.com>
2021-03-14 17:22:35 +0100

    Add test on parsing apt list --upgradable

--- tests/facts/test_apt.py
+++ tests/facts/test_apt.py
@@ -2,7 +2,7 @@ import os.path
 import pytest
 
 from okonf.connectors import LocalHost
-from okonf.facts.apt import AptPresent
+from okonf.facts.apt import AptPresent, AptAbsent, parse_upgradeable
 
 
 @pytest.mark.asyncio
@@ -13,3 +13,25 @@ async def test_AptPresent():
     assert not os.path.isfile('/usr/bin/tree')
     assert await AptPresent('tree').apply(host)
     assert os.path.isfile('/usr/bin/tree')
+
+
+APT_UPGRADABLE = """
+Listing... Done
+juk/testing 4:20.12.3-1 amd64 [upgradable from: 4:20.12.0-1]
+konsole-kpart/testing 4:20.12.3-1 amd64 [upgradable from: 4:20.12.2-1]
+""".split('\n')
+
+
+def test_parse_upgradeable():
+    assert list(parse_upgradeable(APT_UPGRADABLE)) == [
+        ('juk',
+         {'arch': 'amd64',
+          'next_version': '4:20.12.3-1',
+          'source': 'testing',
+          'version': '4:20.12.0-1'}),
+        ('konsole-kpart',
+         {'arch': 'amd64',
+          'next_version': '4:20.12.3-1',
+          'source': 'testing',
+          'version': '4:20.12.2-1'}),
+    ]
