e282d102792225bcde36a71c5bdbd7d116e5c143
Hugo Herter <git@hugoherter.com>
2022-08-12 21:48:10 +0200

    Fix: Docker image had to be built for every test

--- run_tests_docker.sh
+++ run_tests_docker.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+mkdir -p "htmlcov"
+
 docker build -t okonf-test -f Dockerfile .
-docker run --rm --mount type=bind,source="$(pwd)"/htmlcov,target=/opt/htmlcov \
-    okonf-test /opt/run_tests.sh $@
+docker run --rm -ti \
+  --mount type=bind,readonly,source="$(pwd)"/okonf,target=/opt/okonf \
+  --mount type=bind,readonly,source="$(pwd)"/tests,target=/opt/tests \
+  --mount type=bind,source="$(pwd)"/htmlcov,target=/opt/htmlcov \
+    okonf-test $@
