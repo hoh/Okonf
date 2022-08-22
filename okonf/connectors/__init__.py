de630093e225bde1289be8cb8569ce8f80844206
Hugo Herter <git@hugoherter.com>
2022-08-22 12:21:42 +0200

    Remove unused LXD support

--- okonf/connectors/__init__.py
+++ okonf/connectors/__init__.py
@@ -1,15 +1,5 @@
-import logging
-
 from .local import LocalHost
 from .ssh import SSHHost
-
-try:
-    from .lxd import LXDHost
-
-    assert LXDHost
-except ImportError:
-    logger = logging.getLogger(__name__)
-    logger.debug("Clound not import LXD")
 
 assert SSHHost
 assert LocalHost
