50fef5ffda5d309b5f15625d6f93fa6abbf3ccf3
arnaud <arnaud@okeso.be>
2022-11-28 12:10:56 +0100

    Fix: AptPresent check was not working on specific package due to broken regex
    Solution: Removed the \s+ after the version number in regex, in case no version was provided it forced the package name to have at least two whitespaces between name and version which is not the case in case of long package name.
    
    A unit test check if the specific problem still appears.

--- tests/facts/test_apt.py
+++ tests/facts/test_apt.py
@@ -4,6 +4,7 @@ import pytest
 
 from okonf.connectors.local import LocalHost
 from okonf.facts.apt import AptPresent, AptAbsent, parse_upgradeable
+from unittest.mock import patch
 
 
 @pytest.mark.asyncio
@@ -25,6 +26,21 @@ konsole-kpart/testing 4:20.12.3-1 amd64 [upgradable from: 4:20.12.2-1]
 """.split(
     "\n"
 )
+
+
+@patch('okonf.connectors.abstract.Executor.check_output')
+@pytest.mark.asyncio
+async def test_aptPresent_regex(mock_check_output):
+    mock_check_output.return_value = """
+Desired=Unknown/Install/Remove/Purge/Hold
+| Status=Not/Inst/Conf-files/Unpacked/halF-conf/Half-inst/trig-aWait/Trig-pend
+|/ Err?=(none)/Reinst-required (Status,Err: uppercase=bad)
+||/ Name           Version      Architecture Description
++++-==============-============-============-===========================================================
+ii  docker-compose 1.25.0-1     all          Punctual, lightweight development environments using Docker
+        """
+    async with LocalHost() as host:
+        assert await AptPresent("docker-compose").check(host)
 
 
 def test_parse_upgradeable():
